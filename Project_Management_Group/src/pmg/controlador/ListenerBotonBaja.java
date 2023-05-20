28fe590acb08278ee3b72f7f1d0806c619e913db
JorgeBurgos <jorge.burgos.barrera@gmail.com>
2023-05-20 16:44:04 +0200

    Añadidos cambios a Listener Boton y Acceso BD

--- Project_Management_Group/src/pmg/controlador/ListenerBotonBaja.java
+++ Project_Management_Group/src/pmg/controlador/ListenerBotonBaja.java
@@ -43,54 +43,28 @@ public class ListenerBotonBaja implements ActionListener {
 	}
 	
 	public void bajaAlumnos() {
-//		//Se recogen el código generado
-//		String codigo = vab.getCodigo();
-//		//Se crea un acceso a la bbdd y su conexión
-//		acceso = new AccesoBBDD();
-//		Connection con = acceso.getConexion();
-//		//Comprobamos si el expediente es único
-//		if (!acceso.existeExpedienteAlumno(con, datos.getNumExp())) {
-//			//Se modifica el label de la ventana Alumnos_Alta si en función de si se realiza la operación o no
-//			vala.mostrarAviso(acceso.altaAlumno(con, datos)); //el método devuleve true o false			
-//		} else {
-//			vala.getAviso().setText("Número de expediente no válido");
-//			vala.getLblCod().setText("");
-//		}
-//		acceso.cerrarConexion();
+		//Se recoge el código generado
+		String codigo = vab.getCodigo();
+		//Se crea un acceso a la bbdd y su conexión
+		acceso = new AccesoBBDD();
+		Connection con = acceso.getConexion();
+		
+		//Comprobamos si el código es único
+		if (acceso.exiteCodigoAlumno(con, codigo)) {
+			//Se modifica el label de la ventana Alumnos_Baja si en función de si se realiza la operación o no
+			vab.mostrarAviso(acceso.bajaAlumno(con, codigo)); //el método devuleve true o false			
+		} else {
+			vab.getAviso().setText("Número de código no válido");
+		}
+		acceso.cerrarConexion();
 	}
 	
 	public void bajaArea() {
-//		//Se recogen los datos introducidos por el usuario
-//		DatosAltaArea datos = vara.getDatos();
-//		//Se crea un acceso a la bbdd y su conexión
-//		acceso = new AccesoBBDD();
-//		Connection con = acceso.getConexion();
-//		//Comprobamos si el nombre_corto es único
-//		if (!acceso.existeNombreCortoArea(con, datos.getNombre_corto())) {
-//			//Se modifica el label de la ventana Alumnos_Alta si en función de si se realiza la operación o no
-//			vara.mostrarAviso(acceso.altaArea(con, datos)); //el método devuleve true o false			
-//		} else {
-//			vara.getMensaje().setText("Nombre corto no válido");
-//			vara.getAviso().setText("");
-//		}
-//		acceso.cerrarConexion();
+
 	}
 	
 	public void bajaPI() {
-//		//Se recogen los datos introducidos por el usuario
-//		DatosAltaPI datos = vpia.getDatos();
-//		//Se crea un acceso a la bbdd y su conexión
-//		acceso = new AccesoBBDD();
-//		Connection con = acceso.getConexion();
-//		//Comprobamos si el nombre_corto es único
-//		if (!acceso.existeNombrePI(con, datos.getNombre())) {
-//			//Se modifica el label de la ventana Alumnos_Alta si en función de si se realiza la operación o no
-//			vpia.mostrarAviso(acceso.altaPI(con, datos)); //el método devuleve true o false			
-//		} else {
-//			vpia.getLblAviso().setText("Nombre no válido");
-//			vpia.getLblCod().setText("");
-//		}
-//		acceso.cerrarConexion();
+
 	}
 
 }
